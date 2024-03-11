868f663c72f960cfe11443e6f13a9972a2e1f00d
AlexDelei <alexdelei125@gmail.com>
2024-03-11 17:18:34 +0300

    added token urls used by JWT

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,9 +2,16 @@ from django.urls import path
 from . import views
 from rest_framework.authtoken.views import obtain_auth_token
 from django.contrib.auth.admin import UserAdmin
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
     path('auth/', obtain_auth_token),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('', views.api_home),
 ]
